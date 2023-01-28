f1295112daa0e9433b40cf3582475c548f40916d
ghazanfarali345 <ghazanfarmalik345@gmail.com>
2023-01-28 22:40:56 +0500

    film list test

--- apps/film/src/film.controller.spec.ts
+++ apps/film/src/film.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmController } from './film.controller';
+import { Film } from './film.schema';
 import { FilmService } from './film.service';
+import { filmStub } from './test/film.stub';
 import { FilmServiceMock } from './___mock___/film.service.mock';
 
 jest.mock('./film.service');
@@ -21,5 +23,23 @@ describe('FilmController', () => {
     filmController = module.get<FilmController>(FilmController);
     filmService = module.get<FilmService>(FilmService);
     jest.clearAllMocks();
+  });
+
+  describe('getFilms', () => {
+    describe('when getFilmListHandler is called', () => {
+      let films: Film[];
+
+      beforeEach(async () => {
+        films = await filmController.getFilmListHandler();
+      });
+
+      test('then it should call filmService', () => {
+        expect(filmService.getAllFilms).toHaveBeenCalled();
+      });
+
+      test('then it should return films', () => {
+        expect(films).toEqual([filmStub()]);
+      });
+    });
   });
 });
