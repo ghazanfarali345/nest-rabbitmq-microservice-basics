0ebc71a8d79e48177e367393bb84626160d98dfa
ghazanfarali345 <ghazanfarmalik345@gmail.com>
2023-01-29 19:41:02 +0500

    comments in gateway service

--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -14,25 +14,47 @@ export class AppService {
     return 'hello ';
   }
 
-  // Film Services
+  /**
+   * Create Film Sercice
+   * Takes a body and send it film service
+   */
+
   async createFilm(request) {
     this.logger.log('create film request in gateway service', request);
 
     return this.filmClient.send('create_film', { ...request });
   }
 
+  /**
+   *  Film List
+   *  Hits film_list event to get list of films from film service
+   */
   async getFilmList() {
     return this.filmClient.send('film_list', {});
   }
 
+  /**
+   * Film Update by id
+   * Hits film_update event to upadate film by id
+   */
+
   async filmUpdate(id, request) {
     return this.filmClient.send('film_update', { id, ...request });
   }
+
+  /**
+   * Delte film by title
+   * Hits film_delete event for delete film by title (Here i need to add slug insted title)
+   */
+
   async filmDelete(title) {
     return this.filmClient.send('film_delete', title);
   }
 
-  // User Services
+  /**
+   * Create User service
+   * Hits create_user event with body to create user
+   */
 
   async createUser(request) {
     return this.userClient.send('create_user', { ...request });
